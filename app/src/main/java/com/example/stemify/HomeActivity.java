175ef4f3500efb3e09daa2e5ef1c48a87a0eb8fb
kevinhotzewan <kevinhotzewan@gmail.com>
2023-12-17 17:54:15 +0800

    added bottom navigation bar. bug with navigation via bottom bar overlapping

--- app/src/main/java/com/example/stemify/HomeActivity.java
+++ app/src/main/java/com/example/stemify/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.example.stemify;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.example.stemify.ui.moduleA.HomePageA;
+import com.example.stemify.ui.moduleB.HomePageB;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,17 +19,21 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import com.example.stemify.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeBinding binding;
+    private HomePageA homePageA = new HomePageA();
+    private HomePageB homePageB = new HomePageB();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Set up side navigation drawer & app bar
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -41,6 +50,26 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        // Set up bottom navigation bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                CharSequence itemTitle = item.getTitle();
+
+                if(itemTitle.equals("ModA")){
+                    loadFragment(homePageA);
+                    return true;
+                }
+                else if(itemTitle.equals("ModB")){
+                    loadFragment(homePageB);
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
@@ -48,5 +77,11 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.nav_host_fragment_content_home, fragment)
+                .commit();
     }
 }
