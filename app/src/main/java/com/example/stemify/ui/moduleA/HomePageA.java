6b6bc82e1077c5c481c7feadb249692ae15d9f3a
kevinhotzewan <kevinhotzewan@gmail.com>
2023-12-19 01:53:51 +0800

    added tab layout for all modules along with template fragments for all respective modules' subsections

--- app/src/main/java/com/example/stemify/ui/moduleA/HomePageA.java
+++ app/src/main/java/com/example/stemify/ui/moduleA/HomePageA.java
@@ -3,64 +3,41 @@ package com.example.stemify.ui.moduleA;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.stemify.R;
+import com.example.stemify.VPAdapter;
+import com.google.android.material.tabs.TabLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomePageA#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomePageA extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomePageA() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomePageA.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomePageA newInstance(String param1, String param2) {
-        HomePageA fragment = new HomePageA();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page_a, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page_a, container, false);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.modA_TLHomePageA);
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.modA_VPHomePageA);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getParentFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new ResourceLibrary(), "LIBRARY");
+        vpAdapter.addFragment(new ResourceCommunity(),  "COMMUNITY");
+        vpAdapter.addFragment(new Downloads(), "DOWNLOADS");
+        viewPager.setAdapter(vpAdapter);
+
+        return view;
     }
 }
