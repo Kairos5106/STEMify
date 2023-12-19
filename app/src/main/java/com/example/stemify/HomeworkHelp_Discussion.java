382dd46b7a175b293c50ff553f7163a3a99f0271
Aireenelz <u2100667@siswa.um.edu.my>
2023-12-19 18:17:38 +0800

    merge changes from master

---
+++ app/src/main/java/com/example/stemify/HomeworkHelp_Discussion.java
@@ -0,0 +1,14 @@
+package com.example.stemify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeworkHelp_Discussion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework_help_discussion);
+    }
+}
