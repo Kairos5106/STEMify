3ed0a078af22f1ff5e61b9b5ab850380d5302377
Aireenelz <u2100667@siswa.um.edu.my>
2023-12-23 02:15:53 +0800

    fixed BtnAskAQuestion and back stack

--- app/src/main/java/com/example/stemify/HomeworkHelp_NewQuestion.java
+++ app/src/main/java/com/example/stemify/HomeworkHelp_NewQuestion.java
@@ -1,8 +1,9 @@
 package com.example.stemify;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
 
 public class HomeworkHelp_NewQuestion extends AppCompatActivity {
 
@@ -10,5 +11,39 @@ public class HomeworkHelp_NewQuestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homework_help_new_question);
+
+        // Enable back button in the action bar
+        Toolbar toolbar = findViewById(R.id.TBNewQuestion);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // Set the title for the app bar for this particular page
+        setTitle("New Question");
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle the back button click
+        if (item.getItemId() == android.R.id.home) {
+            // Navigate back to the previous fragment or activity
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle the back button click
+        if (item.getItemId() == android.R.id.home) {
+            // Finish the current activity to navigate back
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
 }
