2c291edae7861980bcbe61b4540cab6cb1dacb3e
Aireenelz <u2100667@siswa.um.edu.my>
2023-12-28 17:08:50 +0800

    feature 1/3: user able to create new question and add post to database. however some problems: 1. can't add userPfp data to post data 2. tags feature hasn't been configured

--- app/src/main/java/com/example/stemify/HomeworkHelp_Post.java
+++ app/src/main/java/com/example/stemify/HomeworkHelp_Post.java
@@ -1,5 +1,7 @@
 package com.example.stemify;
 
+import android.net.Uri;
+
 import com.google.firebase.database.ServerValue;
 
 import java.util.List;
@@ -10,16 +12,19 @@ public class HomeworkHelp_Post {
     private String title;
     private String description;
     private String userID;
-    private String userPfp;
+    private Uri userPfp;
     private List<String> tags;
+    private Object timeStamp;
 
-    public HomeworkHelp_Post(String title, String description, String userID, String userPfp) {
+    public HomeworkHelp_Post(String title, String description, String userID, Uri userPfp) {
         this.title = title;
         this.description = description;
         this.userID = userID;
         this.userPfp = userPfp;
+        this.timeStamp = ServerValue.TIMESTAMP;
     }
 
+    // Default constructor
     public HomeworkHelp_Post() {
     }
 
@@ -27,47 +32,55 @@ public class HomeworkHelp_Post {
         return postKey;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public String getUserPfp() {
-        return userPfp;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
     public void setPostKey(String postKey) {
         this.postKey = postKey;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getUserID() {
+        return userID;
     }
 
     public void setUserID(String userID) {
         this.userID = userID;
     }
 
-    public void setUserPfp(String userPfp) {
+    public Uri getUserPfp() {
+        return userPfp;
+    }
+
+    public void setUserPfp(Uri userPfp) {
         this.userPfp = userPfp;
+    }
+
+    public List<String> getTags() {
+        return tags;
     }
 
     public void setTags(List<String> tags) {
         this.tags = tags;
+    }
+
+    public Object getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Object timeStamp) {
+        this.timeStamp = timeStamp;
     }
 }
