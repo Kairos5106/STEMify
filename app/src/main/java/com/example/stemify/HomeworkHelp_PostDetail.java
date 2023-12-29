2ceffa44106df3d1c841dcab858a48909fca4070
Aireenelz <u2100667@siswa.um.edu.my>
2023-12-29 16:05:34 +0800

    reconfigure post details xml layout

--- app/src/main/java/com/example/stemify/HomeworkHelp_PostDetail.java
+++ app/src/main/java/com/example/stemify/HomeworkHelp_PostDetail.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HomeworkHelp_Discussion extends AppCompatActivity {
+public class HomeworkHelp_PostDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homework_help_discussion);
+        setContentView(R.layout.activity_homework_help_post_detail);
     }
 }
