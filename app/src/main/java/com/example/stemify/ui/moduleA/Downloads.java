ed6c2f29a5a830013c3bf4476a95902be87cc911
kevinhotzewan <kevinhotzewan@gmail.com>
2023-12-30 11:35:00 +0800

    found error code.. finding solution

--- app/src/main/java/com/example/stemify/ui/moduleA/Downloads.java
+++ app/src/main/java/com/example/stemify/ui/moduleA/Downloads.java
@@ -2,16 +2,26 @@ package com.example.stemify.ui.moduleA;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.stemify.DownloadAdapter;
+import com.example.stemify.DownloadItem;
 import com.example.stemify.R;
 
-public class Downloads extends Fragment {
+import java.util.List;
 
+public class Downloads extends Fragment {
+    DownloadAdapter downloadAdapter;
+    RecyclerView recyclerView;
+    List<DownloadItem> listOfItems;
 
     public Downloads() {
         // Required empty public constructor
@@ -20,6 +30,7 @@ public class Downloads extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -27,5 +38,20 @@ public class Downloads extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_downloads, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Setup RecyclerView
+        recyclerView = view.findViewById(R.id.RVDownloads);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        recyclerView.setAdapter(new DownloadAdapter(getContext(), listOfItems));
+    }
+
+    public void initalizeData(){
+        // Populate list with download items
+        listOfItems.add(new DownloadItem("Test1"));
     }
 }
