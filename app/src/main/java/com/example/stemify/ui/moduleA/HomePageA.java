1804745817e2a563cd01868138f3eb5a635ab439
kevinhotzewan <kevinhotzewan@gmail.com>
2024-01-03 19:26:08 +0800

    added a loading page for resource community fragment

--- app/src/main/java/com/example/stemify/ui/moduleA/HomePageA.java
+++ app/src/main/java/com/example/stemify/ui/moduleA/HomePageA.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class HomePageA extends Fragment {
+    Handler handler;
     String userIdentity = "";
     VPAdapter vpAdapter;
     TabLayout tabLayout;
@@ -33,9 +35,7 @@ public class HomePageA extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get user identity from database;
-        getUserIdentity();
-        Toast.makeText(getActivity(), "onCreate userIdentity: " + userIdentity, Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
@@ -49,49 +49,14 @@ public class HomePageA extends Fragment {
 
         tabLayout.setupWithViewPager(viewPager);
 
-        return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
         // Assign fragments to tabs
         vpAdapter = new VPAdapter(getChildFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         vpAdapter.addFragment(new ResourceLibrary(), "LIBRARY");
-        vpAdapter.addFragment(getAppropriateLayout(), "COMMUNITY");
+        vpAdapter.addFragment(new LoadingResourceCommunity(), "COMMUNITY");
         vpAdapter.addFragment(new Downloads(), "DOWNLOADS");
         viewPager.setAdapter(vpAdapter);
         vpAdapter.notifyDataSetChanged();
-    }
 
-    public void getUserIdentity(){
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if(currentUser != null){
-            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid());
-            userRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    User user = snapshot.getValue(User.class);
-                    userIdentity = user.getIdentity();
-                    Toast.makeText(getActivity(), "in userIdentity: " + userIdentity, Toast.LENGTH_SHORT).show();
-                }
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-        }
-    }
-
-    // Adding appropriate resource community layout for users of different identities
-    public Fragment getAppropriateLayout(){
-        if(userIdentity.equalsIgnoreCase("Tutor")){ // if user is an Educator, give student fragment
-            Toast.makeText(getActivity(), "Educator View", Toast.LENGTH_SHORT).show();
-            return new ResourceCommunityEducator();
-        }
-        else { // if user is anything but an Educator, give Student fragment
-            Toast.makeText(getActivity(), "Student View", Toast.LENGTH_SHORT).show();
-            return new ResourceCommunity();
-        }
+        return view;
     }
 }
